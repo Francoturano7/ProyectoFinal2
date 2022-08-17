16821225d7973cc101d6b97641a1b3cdeab5b9dc
Franco_Turano <francoturano777@gmail.com>
2022-08-17 20:24:55 -0300

    subimos al repo para entregar

---
+++ productos.js
@@ -0,0 +1,58 @@
+const herramientas=[
+    {
+        nombre: "Martillo",
+        marca:"Tramontina",
+        descripcion:"Galponero 650gs",
+        codigo: 9931,
+        precio: 900
+    },
+    {
+        nombre: "Pinza",
+        marca:"Metz",
+        descripcion:"Aislada 150mm",
+        codigo: 9932,
+        precio: 700
+    },
+    {
+        nombre: "Alicate",
+        marca:"Bahco",
+        descripcion:"Aislado 175mm",
+        codigo: 9933,
+        precio: 650
+    },
+    {
+        nombre: "Maza",
+        marca:"Azzurra",
+        descripcion:"1.500gs",
+        codigo: 9934,
+        precio: 450
+    },
+    {
+        nombre: "Serrucho",
+        marca:"Santa Juana",
+        descripcion:"Mango plastico 500mm",
+        codigo: 9935,
+        precio: 1200
+    },
+    {
+        nombre: "Cinta Metrica",
+        marca:"Bremen",
+        descripcion:"5mts",
+        codigo: 9936,
+        precio: 500
+    },
+    {
+        nombre: "Nivel",
+        marca:"Crossmaster",
+        descripcion:"Aluminio 400mm",
+        codigo: 9937,
+        precio: 450
+    },
+    {
+        nombre: "Llave Ajustable",
+        marca:"Biassoni",
+        descripcion:"Fosfatizada 200mm",
+        codigo: 9938,
+        precio: 1050
+    },
+];
